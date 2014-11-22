2050c9a3f7bdd9fdb0386e1ddda9eafddff0d7b9
kigiri <le.mikmac@gmail.com>
2014-11-22 22:03:56 +0100

    Refactor

--- popup.js
+++ popup.js
@@ -1,41 +1,37 @@
 // Cached Globals and Aliases
 var _tabs = [];
 var _elem = document.all;
-var _append = function (elem) {
-  _elem.list.appendChild(elem);
-};
-
 
 // Activate focus on popup opening
 _elem.search.focus();
 
 function loadTabs(tabs) {
   _tabs = tabs;
-  console.log(tabs);
+  _tabs.forEach(genTab);
 }
 
+// generate and append tab button
+function genTab(tab, idx) {
+  var title = document.createElement('h3');
+  title.innerHTML = tab.title;
 
-chrome.tabs.query({}, function (tabs) {
-  _tabs = tabs;
-  console.log('_tabs', _tabs);
+  var url = document.createElement('span');
+  url.innerHTML = tab.url;
 
-  _tabs.forEach(function (tab, idx) {
-    console.log(tab.url + ' - ' + idx);
-    var title = document.createElement('h3');
-    title.innerHTML = tab.title;
-    var url = document.createElement('span');
-    url.innerHTML = tab.url;
-    var favIcon = document.createElement('div');
-    favIcon.className  = 'fav-icon';
-    favIcon.style.backgroundImage = "url('" + tab.favIconUrl + "')";
+  var favIcon = document.createElement('div');
+  favIcon.className = 'fav-icon';
+  favIcon.style.backgroundImage = "url('" + tab.favIconUrl + "')";
 
-    var button = document.createElement('button');
-    button.id = 'tab-' + idx + 1;
-    console.log(tab);
-    button.appendChild(favIcon);
-    button.appendChild(title);
-    button.appendChild(url);
-    _append(button);
-  });
-});
+  var button = document.createElement('button');
+  button.id = 'tab-' + idx + 1;
+  console.log(tab);
+  button.appendChild(favIcon);
+  button.appendChild(title);
+  button.appendChild(url);
+  _elem.list.appendChild(button);
+}
+
+// Empty query to retrive all tabs
+chrome.tabs.query({}, loadTabs);
+
 
